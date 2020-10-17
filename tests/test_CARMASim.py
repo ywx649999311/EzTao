9d6c372a39fceb3a498255366cee0b2694ba6eb9
Weixiang Yu <wy73@drexel.edu>
2020-10-16 20:00:01 -0400

    update sim test to log scale, add shift check

--- tests/test_CARMASim.py
+++ tests/test_CARMASim.py
@@ -9,7 +9,7 @@ from celerite import GP
 # init kernels
 drw1 = DRW_term(np.log(0.35), np.log(100))
 drw2 = DRW_term(np.log(0.15), np.log(300))
-drw3 = DRW_term(np.log(0.25), np.log(500))
+drw3 = DRW_term(np.log(0.25), np.log(800))
 dho1 = DHO_term(np.log(2), np.log(1.2), np.log(1), np.log(3))
 dho2 = DHO_term(np.log(2), np.log(0.8), np.log(1), np.log(0.5))
 carma30a = CARMA_term(np.log([3, 2.8, 0.8]), np.log([1]))
@@ -22,12 +22,17 @@ def test_simRand():
     # SNR = 10
     for kernel in test_kernels:
         t, y, yerr = gpSimRand(kernel, 10, 365 * 10.0, 150, nLC=100)
-        carma_amp = kernel.get_rms_amp()
+        log_amp = np.log(kernel.get_rms_amp())
 
-        # compute percentage error
+        # compute error in log space
         err = (
-            np.sqrt(np.var(y, axis=1) - np.var(yerr, axis=1)) - carma_amp
-        ) / carma_amp
+            np.log(np.sqrt(np.var(y, axis=1) - np.var(yerr, axis=1))) - log_amp
+        ) / log_amp
 
-        # pass if std of percentage error < 0.25
+        # pass if std of error < 0.25 and shift < 0.25
         assert np.std(err) < 0.25
+        assert np.abs(np.median(err)) < 0.25
+
+
+# test_simRand()
+
