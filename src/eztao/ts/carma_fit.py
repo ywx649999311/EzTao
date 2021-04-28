7facd6ea84928cd184aeae939cf4cb39eb6308a9
Weixiang Yu <wy73@drexel.edu>
2021-04-28 20:35:18 +0000

    Update init functions, add tests

--- src/eztao/ts/carma_fit.py
+++ src/eztao/ts/carma_fit.py
@@ -7,7 +7,7 @@ from math import ceil
 from scipy.optimize import minimize
 import celerite
 from celerite import GP
-from eztao.carma.CARMATerm import DRW_term, DHO_term, CARMA_term, fcoeffs2coeffs
+from eztao.carma.CARMATerm import DRW_term, DHO_term, CARMA_term
 from functools import partial
 
 # change numpy warn setting
@@ -23,14 +23,14 @@ __all__ = [
     "neg_param_ll",
     "neg_lp_flat",
     "drw_log_param_init",
-    "carma_log_param_init",
+    "dho_log_param_init",
     "carma_log_fcoeff_init",
     "sample_carma",
     "scipy_opt",
 ]
 
 
-def neg_fcoeff_ll(fcoeffs, y, gp):
+def neg_fcoeff_ll(log_fcoeffs, y, gp):
     """
     Negative log likelihood function for CARMA specified in the factored poly space.
 
@@ -38,7 +38,7 @@ def neg_fcoeff_ll(fcoeffs, y, gp):
     return inf as probability.
 
     Args:
-        fcoeffs (array(float)): Coefficients (in natural log) of a CARMA model in
+        log_fcoeffs (array(float)): Coefficients (in natural log) of a CARMA model in
             the factored polynomial space.
         y (array(float)): y values of the input time series.
         gp (object): celerite GP object with a proper CARMA kernel.
@@ -51,7 +51,7 @@ def neg_fcoeff_ll(fcoeffs, y, gp):
     neg_ll = np.inf
 
     try:
-        gp.kernel.set_log_fcoeffs(fcoeffs)
+        gp.kernel.set_log_fcoeffs(log_fcoeffs)
         neg_ll = -gp.log_likelihood(y)
     except celerite.solver.LinAlgError as c:
         # print(c)
@@ -62,7 +62,7 @@ def neg_fcoeff_ll(fcoeffs, y, gp):
     return neg_ll
 
 
-def neg_param_ll(params, y, gp):
+def neg_param_ll(log_params, y, gp):
     """
     Negative log likelihood function for CARMA specified in the nominal space.
 
@@ -70,7 +70,7 @@ def neg_param_ll(params, y, gp):
     return inf as probability.
 
     Args:
-        params (array(float)): CARMA parameters.
+        log_params (array(float)): Natural log of CARMA parameters.
         y (array(float)): y values of the input time series.
         gp (object): celerite GP object with a proper CARMA kernel.
 
@@ -81,7 +81,7 @@ def neg_param_ll(params, y, gp):
     neg_ll = np.inf
 
     try:
-        gp.set_parameter_vector(params)
+        gp.set_parameter_vector(log_params)
         neg_ll = -gp.log_likelihood(y)
     except celerite.solver.LinAlgError as c:
         # print(c)
@@ -92,25 +92,25 @@ def neg_param_ll(params, y, gp):
     return neg_ll
 
 
-def flat_prior(params, bounds):
+def flat_prior(log_params, bounds):
     """
-    A flat prior function. Returns 0 if "params" are within the given "bounds",
-    negative infiinity otherwise.
+    A flat prior function. Returns 0 if "log_params" are within the given "bounds",
+    negative infinity otherwise.
 
     Args:
-        params (array(float)): CARMA parameters in natural log.
+        log_params (array(float)): CARMA parameters in natural log.
         bounds (array((float, float)): An array of boundaries.
 
     Returns:
         float: 0 or negative infinity.
     """
 
-    dim = len(params)
+    dim = len(log_params)
     assert bounds.shape == (dim, 2), "Dimension mismatch for the boundaries!"
 
     bounds_idx = [i for i in range(dim) if all(bounds[i])]
     in_bounds = np.array(
-        [(bounds[i, 0] <= params[i] <= bounds[i, 1]) for i in bounds_idx]
+        [(bounds[i, 0] <= log_params[i] <= bounds[i, 1]) for i in bounds_idx]
     )
 
     if all(in_bounds):
@@ -119,12 +119,13 @@ def flat_prior(params, bounds):
         return -np.inf
 
 
-def neg_lp_flat(params, y, gp, bounds=None, mode="fcoeff"):
+def neg_lp_flat(log_params, y, gp, bounds=None, mode="fcoeff"):
     """
     Negative log probability function using a flat prior.
 
     Args:
-        params (array(float)): CARMA parameters in natural log.
+        log_params (array(float)): CARMA parameters (or coefficients of the factored
+            characteristic polynomial) in natural log.
         y (array(float)): y values of the input time series.
         gp (object): celerite GP object with a proper CARMA kernel.
         bounds (array((float, float)): An array of boundaries. Defaults to None.
@@ -140,7 +141,7 @@ def neg_lp_flat(params, y, gp, bounds=None, mode="fcoeff"):
     else:
         neg_ll = neg_fcoeff_ll
 
-    return -flat_prior(params, bounds) + neg_ll(params, y, gp)
+    return -flat_prior(log_params, bounds) + neg_ll(log_params, y, gp)
 
 
 def drw_log_param_init(amp_range, log_tau_range, size=1):
@@ -158,11 +159,20 @@ def drw_log_param_init(amp_range, log_tau_range, size=1):
         array(float): A ndarray of DRW parameters in natural log.
     """
 
-    init_tau = np.exp(
-        np.random.rand(size, 1) * (log_tau_range[1] - log_tau_range[0])
-        + log_tau_range[0]
-    )
-    init_amp = np.random.rand(size, 1) * (amp_range[1] - amp_range[0]) + amp_range[0]
+    # validate inputs
+    assert (
+        len(amp_range) == len(log_tau_range) == 2
+    ), "Dimension mismatch: Check the input ranges!"
+    assert (np.array(amp_range) > 0).all(), "Amplitudes must be positive!"
+
+    amp_scale = amp_range[1] - amp_range[0]
+    log_tau_scale = log_tau_range[1] - log_tau_range[0]
+    assert amp_scale > 0, "Lower bound must be smaller than upper bound!"
+    assert log_tau_scale > 0, "Lower bound must be smaller than upper bound!"
+
+    # draw samples
+    init_tau = np.exp(np.random.rand(size, 1) * log_tau_scale + log_tau_range[0])
+    init_amp = np.random.rand(size, 1) * amp_scale + amp_range[0]
     log_drw_param = np.log(np.hstack((init_amp, init_tau)))
 
     if size == 1:
@@ -171,122 +181,119 @@ def drw_log_param_init(amp_range, log_tau_range, size=1):
         return log_drw_param
 
 
-def carma_log_param_init(p, q, ranges=None, size=1, a=-8.0, b=8.0, shift=0):
+def dho_log_param_init(ar_range=[-6, 10], ma_range=[-10, 2], size=1):
     """
-    Randomly generate CARMA parameters from [a, b) in natural log.
+    Randomly generate DHO coefficients in the space of the factored polynomials
 
     Args:
-        dim (int): For a CARMA(p,q) model, dim=p+q+1.
-        ranges (list, optional): Tuples of custom ranges to draw parameter proposals
-            from. Defaults to None.
-        size (int, optional): The number of the set of CARMA parameters to generate.
+        ar_range(object, optional): The range (in natural log) for DHO AR parameters.
+            Defaults to [-6, 10].
+        ma_range(object, optional): The range (in natural log) for DHO MA parameters.
+            Defaults to [-10, 2].
+        size (int, optional): The number of the set of DHO parameters to generate.
             Defaults to 1.
-        a (float, optional): The lower bound of the ranges, if the range for a specific
-            parameter is not specified. Defaults to -8.0.
-        b (float, optional): The upper bound of the ranges, if the range for a specific
-            parameter is not specified. Defaults to 8.0.
 
     Returns:
-        array(float): A ndarray of CAMRA parameters in natural log.
+        array(float): A ndarray of DHO parameters in natural log.
     """
-    dim = int(p + q + 1)
-    log_param = np.random.rand(size, int(dim))
 
-    if (ranges is not None) and (len(ranges) == int(dim)):
-        for d in range(dim):
-            if all(ranges[d]):
-                scale = ranges[d][1] - ranges[d][0]
-                log_param[:, d] = log_param[:, d] * scale + ranges[d][0]
-            else:
-                log_param[:, d] = log_param[:, d] * (b - a) + a
-    else:
-        log_param = log_param * (b - a) + a
+    ## validate input
+    assert (
+        len(ar_range) == len(ma_range) == 2
+    ), "Dimension mismatch: Check the input ranges!"
 
-    # add shift if amp too large/small
-    log_param[:, p:] = log_param[:, p:] + shift
+    ar_scale = ar_range[1] - ar_range[0]
+    ma_scale = ma_range[1] - ma_range[0]
+    assert ar_scale > 0, "Lower bound must be smaller than upper bound!"
+    assert ma_scale > 0, "Lower bound must be smaller than upper bound!"
+
+    ## draw samples
+    p, q = (2, 1)
+    log_params = np.random.rand(size, 4)
+
+    # adjust ranges
+    log_params[:, :p] = log_params[:, :p] * ar_scale + ar_range[0]
+    log_params[:, p:-1] = log_params[:, p:-1] * ma_scale + ma_range[0]
 
     if size == 1:
-        return log_param[0]
+        return log_params[0]
     else:
-        return log_param
+        return log_params
 
 
-def carma_log_fcoeff_init(p, q, ranges=None, size=1, a=-8.0, b=8.0, shift=0):
+def carma_log_fcoeff_init(
+    p, q, ar_range=[-8, 8], ma_range=[-10, 6], ma_mult_range=[-10, 0], size=1
+):
     """
-    Randomly generate CARMA coefficients in the factored polynomial space from [a, b)
-    in natural log.
+    Randomly generate CARMA coefficients in the space of the factored polynomials
+
+    The default ranges are set for normalized light curves (with a standard deviation
+    of unity).
 
     Args:
         p (int): The p order of a CARMA(p, q) model.
         q (int): The q order of a CARMA(p, q) model.
-        ranges (list, optional): Tuples of custom ranges to draw polynomial coefficient
-            proposals from. Defaults to None.
+        ar_range(object, optional): The range (in natural log) for AR polynomial
+            coefficients. Defaults to [-8, 8].
+        ma_range(object, optional): The range (in natural log) for MA polynomial
+            coefficients. Defaults to [-10, 6].
+        ma_mult_range(object, optional): The range for the MA multiplier (the coefficient
+            of the highest-order term in the MA characteristic polynomial). Defaults to
+            [-10, 0].
         size (int, optional): The number of the set of coefficients to generate.
             Defaults to 1.
-        a (float, optional): The lower bound of the ranges, if the range for a specific
-            coefficient is not specified. Defaults to -8.0.
-        b (float, optional): The upper bound of the ranges, if the range for a specific
-            coefficient is not specified. Defaults to 8.0.
 
     Returns:
-        array(float): A ndarray of CAMRA parameters in natural log.
+        array(float): A ndarray of coeffs for the factored polynomials in natural log.
+
+    .. note:: The notation (index) follows that in Jones et al. (1982). The last
+        coefficient in the returned array is not part of the coefficients,
+        rather a simple multiplying factor of the entire polynomial, which is needed to
+        obtain the nominal CARMA representation.
     """
+    ## validate inputs
+    assert (
+        len(ar_range) == len(ma_range) == len(ma_mult_range) == 2
+    ), "Dimension mismatch: Check the input ranges!"
+
+    ar_scale = ar_range[1] - ar_range[0]
+    ma_scale = ma_range[1] - ma_range[0]
+    ma_mult_scale = ma_mult_range[1] - ma_mult_range[0]
+    assert ar_scale > 0, "Lower bound must be smaller than upper bound!"
+    assert ma_scale > 0, "Lower bound must be smaller than upper bound!"
+    assert ma_mult_scale > 0, "Lower bound must be smaller than upper bound!"
+
+    ## draw samples
     dim = int(p + q + 1)
-    log_coeff = np.random.rand(size, int(dim))
+    log_fcoeffs = np.random.rand(size, int(dim))
 
-    if (ranges is not None) and (len(ranges) == int(dim)):
-        for d in range(dim):
-            if all(ranges[d]):
-                scale = ranges[d][1] - ranges[d][0]
-                log_coeff[:, d] = log_coeff[:, d] * scale + ranges[d][0]
-            else:
-                log_coeff[:, d] = log_coeff[:, d] * (b - a) + a
-    else:
-        log_coeff = log_coeff * (b - a) + a
+    # set AR, MA range
+    log_fcoeffs[:, :p] = log_fcoeffs[:, :p] * ar_scale + ar_range[0]
+    log_fcoeffs[:, p:-1] = log_fcoeffs[:, p:-1] * ma_scale + ma_range[0]
 
-    # if range for highest order MA not specified
-    if (ranges is None) or (not all(ranges[-1])):
-        perturb = np.random.rand(size, 1) * 10 - 5
-        log_ma_coeff = log_coeff[:, p:]
-        low_term = np.zeros((size, 1))
-
-        if q > 0:
-            if q % 2 == 1:
-                low_term += log_ma_coeff[:, -2][:, np.newaxis]
-            for i in range(1, q, 2):
-                low_term += log_ma_coeff[:, i][:, np.newaxis]
-
-        # update higher order MA
-        log_coeff[:, -1] = -low_term[:, 0] + perturb[:, 0]
-        # log_coeff[:, -1] = -low_term[:, 0]
-        log_coeff[:, -1] += shift
+    # set ma_mult range
+    log_fcoeffs[:, -1] = log_fcoeffs[:, -1] * ma_mult_scale + ma_mult_range[0]
 
     if size == 1:
-        return log_coeff[0]
+        return log_fcoeffs[0]
     else:
-        return log_coeff
+        return log_fcoeffs
 
 
-def sample_carma(p, q, ranges=None, a=-6, b=6, shift=0):
+def sample_carma(p, q):
     """
     Randomly generate a stationary CARMA model given the orders (p and q).
 
     Args:
         p (int): The p order of a CARMA(p, q) model.
         q (int): The q order of a CARMA(p, q) model.
-        ranges (list): Tuple of custom ranges to draw polynomial coefficients
-            from. Defaults to None.
 
     Returns:
         AR and MA coefficients in two separate arrays.
     """
-    init_fcoeffs = np.exp(
-        carma_log_fcoeff_init(p, q, ranges=ranges, a=a, b=b, shift=shift)
-    )
-    ARpars = fcoeffs2coeffs(np.append(init_fcoeffs[:p], [1]))[1:]
-    MApars = fcoeffs2coeffs(init_fcoeffs[p:])[::-1]
-
-    return ARpars, MApars
+    init_log_fcoeffs = carma_log_fcoeff_init(p, q)
+    logAR, logMA = CARMA_term.fcoeffs2carma_log(init_log_fcoeffs, p)
+    return np.exp(logAR), np.exp(logMA)
 
 
 def scipy_opt(
@@ -301,7 +308,7 @@ def scipy_opt(
     opt_options={},
 ):
     """
-    A wrapper for scipy.optimize.minimze method.
+    A wrapper for scipy.optimize.minimize method.
 
     Args:
         y (array(float)): y values of the input time series.
@@ -316,9 +323,9 @@ def scipy_opt(
             should be determined in the "_fit" functions based on the value of the p
             order. Defaults to "fcoeff".
         debug (bool, optional): Turn on/off debug mode. Defaults to False.
-        opt_kwargs (dict, optional): Keyword arguments for scipy.optimize.minimze.
+        opt_kwargs (dict, optional): Keyword arguments for scipy.optimize.minimize.
             Defaults to {}.
-        opt_options (dict, optional): "options" argument for scipy.optimize.minimze.
+        opt_options (dict, optional): "options" argument for scipy.optimize.minimize.
             Defaults to {}.
 
     Returns:
