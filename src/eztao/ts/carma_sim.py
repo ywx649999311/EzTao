a7d285b5902827ed29b19009e8e11476cf1841c7
Weixiang Yu <wy73@drexel.edu>
2024-07-08 19:59:20 +0000

    note: fixed #79 #49
    #79
    #49

--- src/eztao/ts/carma_sim.py
+++ src/eztao/ts/carma_sim.py
@@ -181,9 +181,6 @@ def gpSimByTime(carmaTerm, SNR, t, factor=10, nLC=1, log_flux=True, lc_seed=None
         SNR (float): Signal-to-noise defined as ratio between CARMA RMS amplitude and
             the median of the measurement errors (simulated using log normal).
         t (array(float)): The desired time stamps (starting from zero).
-        factor (int, optional): Parameter to control the ratio in the sampling
-            rate between the simulated full time series and the desired output one.
-            Defaults to 10.
         nLC (int, optional): Number of time series to simulate. Defaults to 1.
         log_flux (bool): Whether the flux/y values are in astronomical magnitude.
             This argument affects how errors are assigned. Defaults to True.
@@ -193,29 +190,43 @@ def gpSimByTime(carmaTerm, SNR, t, factor=10, nLC=1, log_flux=True, lc_seed=None
         (array(float), array(float), array(float)): Time stamps (default in day), y
         values and measurement errors of the simulated time series.
     """
-    # get number points in full LC based on desired cadence
-    duration = ceil(t[-1] - t[0])
-    N = int(factor * ceil(duration / np.median(t[1:] - t[:-1])))
+    if lc_seed is not None:
+        rng = np.random.default_rng(seed=lc_seed)
+    else:
+        rng = np.random.default_rng()
 
-    # simulate full LC
-    tFull, yFull, yerrFull = gpSimFull(
-        carmaTerm, SNR, duration, N=N, nLC=nLC, log_flux=log_flux, lc_seed=lc_seed
-    )
-    tFull = np.atleast_2d(tFull)
-    yFull = np.atleast_2d(yFull)
-    yerrFull = np.atleast_2d(yerrFull)
+    t = np.atleast_1d(t)
+    noise = carmaTerm.get_rms_amp() / SNR
+    yerr = rng.lognormal(0, 0.25, len(t)) * noise
+    yerr = yerr[np.argsort(np.abs(yerr))]  # small->large
 
-    # downsample by desired output cadence
-    t_expand = np.repeat(t[None, :], nLC, axis=0)
-    tOut_idx = np.array(list(map(downsample_byTime, tFull, t_expand)))
-    tOut = np.array(list(map(lambda x, y: x[y], tFull, tOut_idx)))
-    yOut = np.array(list(map(lambda x, y: x[y], yFull, tOut_idx)))
-    yerrOut = np.array(list(map(lambda x, y: x[y], yerrFull, tOut_idx)))
+    # init GP and solve matrix
+    gp_sim = GP(carmaTerm)
+    gp_sim.compute(t)
+
+    # simulate, assign yerr based on y
+    t = np.repeat(t[None, :], nLC, axis=0)
+    y = gp_sample(gp_sim, size=nLC, seed=lc_seed)
+
+    print
+
+    # format yerr making it heteroscedastic
+    yerr = np.repeat(yerr[None, :], nLC, axis=0)
+
+    # if in mag, large value with large error; in flux, the opposite
+    if log_flux:
+        # ascending sort
+        y_rank = y.argsort(axis=1).argsort(axis=1)
+        yerr = np.array(list(map(lambda x, y: x[y], yerr, y_rank)))
+    else:
+        # descending sort
+        y_rank = (-y).argsort(axis=1).argsort(axis=1)
+        yerr = np.array(list(map(lambda x, y: x[y], yerr, y_rank)))
 
     if nLC == 1:
-        return tOut[0], yOut[0], yerrOut[0]
+        return t[0], y[0], yerr[0]
     else:
-        return tOut, yOut, yerrOut
+        return t, y, yerr
 
 
 def addNoise(y, yerr, seed=None):
